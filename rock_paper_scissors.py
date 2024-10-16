cffd8abb439ab66c75512e1dd8db24e9acb8d0c9
Rishi <rishipatel1021@gmail.com>
2024-10-15 23:54:52 -0500

    started rock paper scisors project game

---
+++ rock_paper_scissors.py
@@ -0,0 +1,19 @@
+# ENDED at 42:55 for YT Video, 9 HOURS OF Python Projects 
+#always have imports at the top of the program
+
+import random
+
+user_wins = 0
+computer_wins = 0 
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        quit()
+
+
+
+
+
+
+
